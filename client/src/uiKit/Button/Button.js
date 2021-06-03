fc006b4533db32bc4e97ca3c9900311c5b222ede
Jenny Ali <jenny_p_c@hotmail.com>
2021-06-03 13:36:04 +0100

    styles added // buttonJS started

--- client/src/uiKit/Button/Button.js
+++ client/src/uiKit/Button/Button.js
@@ -1,24 +1,76 @@
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
+import './button.css';
 
-function Button({ text, variant, onClick }) {
+const Button = ({
+  text,
+  type,
+  variant = 'default',
+  icon,
+  className,
+  onClick,
+  large,
+  small,
+  left,
+  right,
+}) => {
   return (
-    <button type="button" className={classnames(variant)} onClick={onClick}>
-      {text}
+    <button
+      className={classnames(
+        'btn',
+        className,
+        type,
+        variant,
+        { istype: type },
+        { '--large': large },
+        { '--small': small }
+      )}
+      onClick={onClick}
+    >
+      {icon && left && <span className="btn_icon">{icon}</span>}
+      {icon && !left && !right && <span className="btn_icon">{icon}</span>}
+      {text && (
+        <span className={classnames('btn_text', { '--right': right }, { '--left': left })}>
+          {text}
+        </span>
+      )}
+      {icon && right && <span className="btn_icon">{icon}</span>}
     </button>
   );
-}
+};
 
 Button.propTypes = {
   text: PropTypes.string,
-  variant: PropTypes.oneOf(['primary', 'secondary']),
+  type: PropTypes.oneOf([
+    'primary',
+    'primary-invert',
+    'secondary',
+    'secondary-invert',
+    'grey',
+    'grey-altcolor',
+    'submit',
+  ]),
+  variant: PropTypes.oneOf(['default', 'flat', 'transparent']),
+  icon: PropTypes.element,
+  className: PropTypes.string,
   onClick: PropTypes.func,
+  large: PropTypes.bool,
+  small: PropTypes.bool,
+  left: PropTypes.bool,
+  right: PropTypes.bool,
 };
 
 Button.defaultProps = {
-  text: '',
-  variant: 'primary',
+  text: 'Button',
+  type: 'primary',
+  variant: 'default',
+  icon: null,
+  className: '',
   onClick: null,
+  large: false,
+  small: false,
+  left: false,
+  right: false,
 };
 
 export default Button;
