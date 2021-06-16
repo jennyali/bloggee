6a3a98c054df93a615967249b1d1cc67b2d38ee1
Jenny Ali <jenny_p_c@hotmail.com>
2021-06-16 10:30:20 +0100

    code completion

--- client/src/posts/RecentPostsList/RecentPostsList.js
+++ client/src/posts/RecentPostsList/RecentPostsList.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import _ from 'lodash';
 import './RecentPostsList.css';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
 
-const RecentPostsList = ({ title, content, numberOf }) => {
+const RecentPostsList = ({ title, content, numberOf, className }) => {
   const sampledList = _.sampleSize(content, numberOf);
 
   const renderedList = _.map(sampledList, function (item, i) {
@@ -10,11 +12,25 @@ const RecentPostsList = ({ title, content, numberOf }) => {
   });
 
   return (
-    <div className="post-list default">
+    <div className={classnames('post-list', 'default', className)}>
       <h3 className="post-list__title">{title}</h3>
       <ul className="post-list__list primary_li">{renderedList}</ul>
     </div>
   );
+};
+
+RecentPostsList.propTypes = {
+  title: PropTypes.string,
+  content: PropTypes.oneOfType([PropTypes.array, PropTypes.object]),
+  numberOf: PropTypes.number,
+  className: PropTypes.string,
+};
+
+RecentPostsList.defaultProps = {
+  title: 'Recent Posts',
+  content: [],
+  numberOf: 0,
+  className: null,
 };
 
 export default RecentPostsList;
